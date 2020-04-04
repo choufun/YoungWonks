4a47ba427961d2f232b4488d94d51f1d50762132
Steven Chou <choufun69@gmail.com>
2020-04-04 03:02:26 -0700

    Added Handler for GET Routing

--- Level 3/Sockets/Lesson 4 - Web Server/server.py
+++ Level 3/Sockets/Lesson 4 - Web Server/server.py
@@ -11,15 +11,55 @@ while True:
         ch=browser.recv(1).decode('utf-8')
         message=message+ch
     print("Message from browser: "+message)
-    browser.sendall(b"""HTTP/1.1 200 OK\r\n
-            <!DOCTYPE html>
-                <html>
-                    <head>
-                        <title>Python Web Server</title>
-                        <h1>Python Web Server</h1>
-                    </head>
-                    <body>
-                        <p>A Python web server is serving this webpage.</p>
-                    </body>
-                </html>""")
+    print(list(message))
+
+    if message == "GET / HTTP/1.1\r\n":
+        browser.sendall(b"""HTTP/1.1 200 OK\r\n
+                <!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>Python Web Server</title>
+                            <h1>Python Web Server</h1>
+                        </head>
+                        <body>
+                            <p>A Python web server is serving this webpage.</p>
+                            <br>
+                            <a href='http://127.0.0.1:1234/newpage'> 
+                                <button>Click Here</button> 
+                            </a>
+                        </body>
+                    </html>""")
+    elif message == "GET /newpage HTTP/1.1\r\n":
+        browser.sendall(b"""HTTP/1.1 200 OK\r\n
+                <!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>Python Web Server</title>
+                            <h1>Python Web Server</h1>
+                        </head>
+                        <body>
+                            <p>This is a new page serving this Python Web Server.</p>
+                            <br>
+                            <a href='http://127.0.0.1:1234'> 
+                                <button>Go Back</button> 
+                            </a>
+                            <br>
+                            <a href='http://127.0.0.1:1234/pagenotfound'> 
+                                <button>Click Here</button> 
+                            </a>
+                        </body>
+                    </html>""")
+    else:
+        browser.sendall(b"""HTTP/1.1 200 OK\r\n
+                <!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>Python Web Server</title>
+                            <h1>Python Web Server</h1>
+                        </head>
+                        <body>
+                            <p>Web Page Not Found.</p>
+                        </body>
+                    </html>""")
+        
 browser.close()
